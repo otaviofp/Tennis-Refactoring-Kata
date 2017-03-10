06934e51e249e8efcd7072214b52be5da01aaa7e
opereira <opereira@microsoft.com>
2017-03-10 11:39:49 -0800

    Extracting player concept

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -1,33 +1,42 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TennisGame1.cs" company="Microsoft" >
+//   Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
 namespace Tennis
 {
+    class Player
+    {
+        private string name;
+
+        public int Score { get; set; }
+
+        public Player(string name)
+        {
+            this.name = name;
+            this.Score = 0;
+        }
+    }
+
     class TennisGame1 : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
-        private string player1Name;
-        private string player2Name;
+        private Player player1;
+
+        private Player player2;
 
         public TennisGame1(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
-        }
-
-        public void WonPoint(string playerName)
-        {
-            if (playerName == "player1")
-                m_score1 += 1;
-            else
-                m_score2 += 1;
+            this.player1 = new Player(player1Name);
+            this.player2 = new Player(player2Name);
         }
 
         public string GetScore()
         {
-            string score = "";
+            string score = string.Empty;
             var tempScore = 0;
-            if (m_score1 == m_score2)
+            if (this.player1.Score == this.player2.Score)
             {
-                switch (m_score1)
+                switch (this.player1.Score)
                 {
                     case 0:
                         score = "Love-All";
@@ -41,12 +50,11 @@ namespace Tennis
                     default:
                         score = "Deuce";
                         break;
-
                 }
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            else if (this.player1.Score >= 4 || this.player2.Score >= 4)
             {
-                var minusResult = m_score1 - m_score2;
+                var minusResult = this.player1.Score - this.player2.Score;
                 if (minusResult == 1) score = "Advantage player1";
                 else if (minusResult == -1) score = "Advantage player2";
                 else if (minusResult >= 2) score = "Win for player1";
@@ -56,8 +64,13 @@ namespace Tennis
             {
                 for (var i = 1; i < 3; i++)
                 {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
+                    if (i == 1) tempScore = this.player1.Score;
+                    else
+                    {
+                        score += "-";
+                        tempScore = this.player2.Score;
+                    }
+
                     switch (tempScore)
                     {
                         case 0:
@@ -75,8 +88,14 @@ namespace Tennis
                     }
                 }
             }
+
             return score;
+        }
+
+        public void WonPoint(string playerName)
+        {
+            if (playerName == "player1") this.player1.Score += 1;
+            else this.player2.Score += 1;
         }
     }
 }
-
